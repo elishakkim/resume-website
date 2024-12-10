7d68dd4af4c79cf47b800607aa69a80d722db693
Elisha <elishakimcoding@gmail.com>
2024-12-09 22:37:41 -0500

    Update Email

--- src/files/InfoConstants.js
+++ src/files/InfoConstants.js
@@ -1,7 +1,7 @@
 export const InfoConstants = {
   summary: 'Experienced full-stack software engineer who enjoys software and solutions architecting seeking opportunities on a fast-paced team to help lead and contribute expertise for the growth of the organization.',
   location: 'Atlanta, GA, 30324',
-  email: 'elishakkim@gmail.com',
+  email: 'elishakimcareer@gmail.com',
   phone: '(678) 823-1935',
   universityTime: '2012 - 2016'
 }
